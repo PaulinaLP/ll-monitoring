81b11219e7e09e95d03fcd9ef0d669fb839a5916
Paulina <pzl.paulina@gmail.com>
2024-07-31 13:33:58 +0200

    first commit

---
+++ monitoring.py
@@ -0,0 +1,10 @@
+import requests 
+
+base_url = 'https://github.com/DataTalksClub/llm-zoomcamp/blob/main'
+relative_url = '03-vector-search/eval/documents-with-ids.json'
+docs_url = f'{base_url}/{relative_url}?raw=1'
+docs_response = requests.get(docs_url)
+documents = docs_response.json()
+top300= documents.head(300)
+top300.to_csv("top300.csv")
+print("done")
